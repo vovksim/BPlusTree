d5f2c6b622a56bd69054e4171f7090f0c698b84e
Ivan Mykhailov <ivan.mykhailov11@gmail.com>
2024-06-12 16:19:13 +0300

    Initial commit. End of work.

---
+++ main.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+#include "bPlusTree.h"
+
+using namespace std;
+
+
+int main()
+{
+    BPlusTree<double> bpt(2);
+    bpt.insert(10);
+    bpt.insert(6);
+    bpt.insert(15);
+    bpt.insert(31);
+    bpt.insert(20);
+    bpt.insert(27);
+    bpt.insert(50);
+    bpt.insert(44);
+    bpt.insert(18);
+    bpt.insert(19);
+    bpt.insert(16);
+    bpt.print(std::cout);
+    return 0;
+}
+
+
